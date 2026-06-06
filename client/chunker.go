339d99c142d0595f514625f90db54b29df0c9336
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2026-06-06 13:50:48 +0100

    hooked up our cli client so it can speak the handshake protocol I defined for the server

--- client/chunker.go
+++ client/chunker.go
@@ -6,21 +6,119 @@ import (
 	"io"
 	"os"
 )
-// CopyFileAndHash reads a source file in 4KB chunks, writes them to a destination file, and calculates the SHA-256 hash of the content on the fly. It returns the hexadecimal represenation of the calculated SHA-256 hash. 
-func CopyFileAndHash(srcPath string, dstPath string) (string, error) {
-	//open the source file for reading 
-	//os.Open returns an *os.file pointer and an error 
-	srcFile, err := os.Open(srcPath)
+
+// ComputeFileHash calculates the SHA-256 has of a local file
+func ComputeFileHash(filepath string) (string, error) {
+	file, err := os.Open(filepath)
 	if err != nil {
-		return "", fmt.Errorf("Failed to Open sorce file: %v", 	err)		
+		return "", err
+	}
+	defer file.Close()
+
+	hasher := sha256.New()
+	buffer := make([]byte, 4096)
+
+	for {
+		n, err := file.Read(buffer)
+		if n > 0 {
+			hasher.Write(buffer[:n])
+		}
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return "", err
+		}
 	}
 
-	//defer tells Go to run this function right before the surrounding function returns, this ensures we always close the file and free up system resources. 
+	return fmt.Sprintf("%x", hasher.Sum(nil)), nil
+}
+
+// SendFileStream reads a file from disk and streams its bytes into the network connection.
+func SendFileStream(filePath string, writer io.Writer) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("Failed to open file for streaming: %v", err)
+
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 4096)
+
+	for {
+		n, err := file.Read(buffer)
+		if n > 0 {
+			//Write the chunk directly to the network connection (writer)
+			_, writeErr := writer.Write(buffer[:n])
+			if writeErr != nil {
+				return fmt.Errorf("failed to write chunk to network: %v", writeErr)
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read file: %v", err)
+		}
+	}
+
+	return nil
+}
+
+//RecieverFileStream reads bytes from the network connection (reader) and writes them to the disk, while calculating the SHA-256 hash on the fly
+
+func RecieverFileStream(dstPath string, reader io.Reader) (string, error) {
+	file, err := os.Create(dstPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to create destination file: %v", err)
+	}
+	defer file.Close()
+
+	hasher := sha256.New()
+	//use our favorite Y-splitter to write to the file and hasher simultaneously!
+	multiWriter := io.MultiWriter(file, hasher)
+
+	buffer := make([]byte, 4096)
+
+	for {
+		//Read a chunk from the network connection
+		n, err := reader.Read(buffer)
+		if n > 0 {
+			//Write the chunk to both files and hasher
+			_, writeErr := multiWriter.Write(buffer[:n])
+			if writeErr != nil {
+				return "", fmt.Errorf("failed to write chunk to disk/hasher: %v", writeErr)
+			}
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return "", fmt.Errorf("network error during read: %v", err)
+		}
+	}
+	// Return the final hash
+	return fmt.Sprintf("%x", hasher.Sum(nil)), nil
+}
+
+// CopyFileAndHash reads a source file in 4KB chunks, writes them to a destination file, and calculates the SHA-256 hash of the content on the fly. It returns the hexadecimal represenation of the calculated SHA-256 hash.
+func CopyFileAndHash(srcPath string, dstPath string) (string, error) {
+	//open the source file for reading
+	//os.Open returns an *os.file pointer and an error
+	srcFile, err := os.Open(srcPath)
+	if err != nil {
+		return "", fmt.Errorf("Failed to Open sorce file: %v", err)
+	}
+
+	//defer tells Go to run this function right before the surrounding function returns, this ensures we always close the file and free up system resources.
 	defer srcFile.Close()
 
-	//Create the destination file for writing 
+	//Create the destination file for writing
 	dstFile, err := os.Create(dstPath)
-	if err != nil{
+	if err != nil {
 		return "", fmt.Errorf("Failed to create destination file: %v", err)
 	}
 
@@ -29,14 +127,14 @@ func CopyFileAndHash(srcPath string, dstPath string) (string, error) {
 	//Create a SHA-256 hasher
 	hasher := sha256.New()
 
-	//Combine the destination file and the hasher using a MultiWriter 
-	//Any write to multiWriter will write to both dstFile and hasher 
+	//Combine the destination file and the hasher using a MultiWriter
+	//Any write to multiWriter will write to both dstFile and hasher
 
 	multiWriter := io.MultiWriter(dstFile, hasher)
 
-	//Create our 4kb byte buffer 
-	//make() is used to allocate slices, maps, and channels 
-	//byte is an alias for uint8 (an 8-bit unsigned integer). 
+	//Create our 4kb byte buffer
+	//make() is used to allocate slices, maps, and channels
+	//byte is an alias for uint8 (an 8-bit unsigned integer).
 	buffer := make([]byte, 4096)
 
 	var totalBytesRead int64
@@ -44,22 +142,22 @@ func CopyFileAndHash(srcPath string, dstPath string) (string, error) {
 	//Start the chunking loop
 
 	for {
-		//Read up to 4096 bytes from the source file into our buffer. 
+		//Read up to 4096 bytes from the source file into our buffer.
 		//n is the actual number of bytes read (could be less than 4096 at the end of the file).
 		n, readErr := srcFile.Read(buffer)
 		if n > 0 {
-			//Write Exactly the n bytes we read (buffer[:n]) to our multiWriter. 
+			//Write Exactly the n bytes we read (buffer[:n]) to our multiWriter.
 			//IF we wrote the whole buffer, we might write leftover garbage bytes!
 			_, writeErr := multiWriter.Write(buffer[:n])
 			if writeErr != nil {
 				return "", fmt.Errorf("failed to write data: %v", writeErr)
 			}
-			totalBytesRead += int64(n) 
+			totalBytesRead += int64(n)
 		}
 
 		//Check if we hit the End of file (EOF)
 		if readErr == io.EOF {
-			break //Exit the loop safely 
+			break //Exit the loop safely
 		}
 
 		if readErr != nil {
@@ -67,14 +165,14 @@ func CopyFileAndHash(srcPath string, dstPath string) (string, error) {
 		}
 	}
 
-		// Get the final SHA-256 hash bytes.
+	// Get the final SHA-256 hash bytes.
 	// Sum(nil) appends the current hash to the passed slice (nil means we just want the hash itself).
 	hashBytes := hasher.Sum(nil)
 
-	//Convert the bytes to a hexadecimal string. 
-	// %x formats bytes as a hex string (e.g "a3f5b2..."). 
+	//Convert the bytes to a hexadecimal string.
+	// %x formats bytes as a hex string (e.g "a3f5b2...").
 	hashString := fmt.Sprintf("%x", hashBytes)
 
 	fmt.Printf("Prototype: Successfully copied %d bytes. \n", totalBytesRead)
-	return hashString, nil 
+	return hashString, nil
 }
--- server/server.go
+++ server/server.go
@@ -1,11 +1,202 @@
 package server
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"math/rand"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 
-// Start kicks off our signaling and relay server.
+type TransferSession struct {
+	fileName     string
+	fileSize     int64
+	fileHash     string
+	senderConn   net.Conn
+	receiverChan chan net.Conn // Used to pass the receiver's connection to the sender's goroutine
+}
+
+// Global state to store the sessions . Maps code -> Session.
+var (
+	sessions  = make(map[string]*TransferSession)
+	sessionsMu sync.Mutex //lock to ensure only one client updates the sessions map at a time, thereby protecting the sessions map from concurrent access crashes
+
+)
+
+// Start starts the TCP signaling and relay server.
 
 func Start(port string) {
-	fmt.Printf("Server: Starting signing & relay server on port %s....\n", port) 
+	//Listen on all interfaces on the specified TCP port
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Printf("Sever Error: Failed to start server: %v\n", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Printf("Server: Listener on port %s...\n", port)
+
+	//Loop forever, accepting clients connections
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Server Error: Failed to accept connection: %v\n", err)
+			continue
+		}
+
+		//Handle each connection concurrently in a background thread (goroutine)
+		go handleConnection(conn)
+
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	//Ensure we close the connection if we exit early
+	//Note: If the connection is handed over to the relay, the relay will handle closing
+
+	defer func() {
+		if conn != nil {
+			conn.Close()
+		}
+	}()
+
+	//Read the first line of text from the connection
+	reader := bufio.NewReader(conn)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	//Trim the newline Characters and split by spaces
+	line = strings.TrimSpace(line)
+	parts := strings.Split(line, " ")
+	if len(parts) < 2 {
+		fmt.Fprintln(conn, "Error invalid command structure")
+		return
+	}
+
+	command := parts[0]
+
+	switch command {
+	case "SHARE":
+		//Format: Share <filename> <filesize> <filehash>
+		if len(parts) < 4 {
+			fmt.Fprintln(conn, "Error usage: Share <filename> <filesize> <filehash>")
+			return
+		}
+		handleShare(conn, parts[1], parts[2], parts[3])
+		conn = nil //set to nil so defer doesnt close it; handle share will handle closing
+
+	case "DOWNLOAD":
+		//Format: Download <code>
+
+		handleDownload(conn, parts[1])
+		conn = nil // Set to nil so defer doesn't close it; handleDownload will handle close
+
+	default:
+		fmt.Fprintln(conn, "Error unknown command")
+
+	}
+}
+
+func handleShare(senderConn net.Conn, fileName string, sizeStr string, fileHash string) {
+	defer senderConn.Close()
+
+	//Convert file size from string to integer
+	size, err := strconv.ParseInt(sizeStr, 10, 64)
+	if err != nil {
+		fmt.Fprintln(senderConn, "Error invalid file size")
+		return
+	}
+	//Generate a 6-digit code
+	code := generateCode()
+
+	//create a new session
+	session := &TransferSession{
+		fileName:     fileName,
+		fileSize:     size,
+		fileHash:     fileHash,
+		senderConn:   senderConn,
+		receiverChan: make(chan net.Conn, 1), //Buffered channel size 1
+	}
+
+	//save to global map (protected by Mutex lock)
+	sessionsMu.Lock()
+	sessions[code] = session
+	sessionsMu.Unlock()
+
+	fmt.Printf("Server: Code %s generated for file %s (%d bytes)\n", code, fileName, size)
+
+	//send the code back to the sender
+	fmt.Fprintf(senderConn, "CODE %s\n", code)
+
+	//Block here and wait for a reciever to join
+	//This will wait until handleDownload 	writes to session.recieverChan.
+	//We set a 5-minute timeout so sessions dont hang forever if no one downloads
+	select {
+	case receiverConn := <-session.receiverChan:
+		defer receiverConn.Close()
+
+		//Tell the server to start sending raw file bytes
+		fmt.Fprintln(senderConn, "START")
+		fmt.Printf("Server: Transfer started for code %s\n", code)
+
+		//relay bytes from sender to reciever
+		bytesCopied, err := io.Copy(receiverConn, senderConn)
+		if err != nil {
+			fmt.Printf("Server Error: Transfer failed for code %s: %v\n", code, err)
+		} else {
+			fmt.Printf("Server: Transfer completed for code %s (%d bytes relayed)\n", code, bytesCopied)
+		}
+
+	case <-time.After(5 * time.Minute):
+		fmt.Printf("Server: Session %s timed out waiting for receiver\n", code)
+		fmt.Fprintln(senderConn, "ERROR timeout waiting for receiver")
+	}
+
+	//Clean up map
+	sessionsMu.Lock()
+	delete(sessions, code)
+	sessionsMu.Unlock()
+
+}
+
+func handleDownload(receiverConn net.Conn, code string) {
+	//Look up session (protected by Lock)
+	sessionsMu.Lock()
+	session, exists := sessions[code]
+	sessionsMu.Unlock()
+
+	if !exists {
+		fmt.Fprintln(receiverConn, "Error session not found")
+		receiverConn.Close()
+		return
+	}
+
+	//send metadata to reciever
+	fmt.Fprintf(receiverConn, "METADATA %s %d %s\n", session.fileName, session.fileSize, session.fileHash)
+
+	//Read "READY\n" from receiver
+	reader := bufio.NewReader(receiverConn)
+	line, err := reader.ReadString('\n')
+	if err != nil || strings.TrimSpace(line) != "READY" {
+		receiverConn.Close()
+		return
+	}
+
+	//send the reciever's connection to the sender's goroutine.
+	//This wakes up the select block in the handleShare !
+	session.receiverChan <- receiverConn
+}
+
+// generateCode generates a temporary 6-digit random code
+func generateCode() string {
+	rand.Seed(time.Now().UnixNano())
+	code := rand.Intn(900000) + 100000 //100000 to 999999 (I have questions here )
+	return fmt.Sprintf("%d", code)
 }
